2a39f39ca90866191ea6fd4f9ab86ea2bfe87ac9
unknown <mada.lawliet@gmail.com>
2023-01-26 14:20:42 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text" placeholder="Enter username..." />
+        <input type="submit" value="Search" />
+      </form>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
